3f3c9a7bfb93e5d318e6e910b8a17f24d4dc7cf0
Mohammad <mj.alijani@gmail.com>
2026-09-01 17:17:32 +0330

    fix(edgeToEdge): handle edgeToEdge for android 7

--- ocr-sdk/src/main/java/ir/ayantech/ocr_sdk/tools/OcrEdgeToEdgeHelper.kt
+++ ocr-sdk/src/main/java/ir/ayantech/ocr_sdk/tools/OcrEdgeToEdgeHelper.kt
@@ -1,14 +1,16 @@
 package ir.ayantech.ocr_sdk.tools
 
-import android.util.TypedValue
+import android.graphics.Color
+import android.os.Build
 import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
@@ -19,24 +21,44 @@ internal class OcrEdgeToEdgeHelper(
     private val activity: ComponentActivity
 ) {
     @ColorInt
-    private val statusBarColor = resolveThemeColor(androidx.appcompat.R.attr.colorPrimary)
+    private val statusBarColor =
+        ContextCompat.getColor(activity, R.color.ocr_header_background)
 
     @ColorInt
     private val navigationBarColor =
         ContextCompat.getColor(activity, R.color.ocr_fragment_background)
 
+    private val useDarkStatusBarIcons = ColorUtils.calculateLuminance(statusBarColor) > 0.5
+    private val useDarkNavigationBarIcons =
+        ColorUtils.calculateLuminance(navigationBarColor) > 0.5
+
+    @ColorInt
+    private val legacyNavigationBarColor = if (useDarkNavigationBarIcons) {
+        ColorUtils.blendARGB(navigationBarColor, Color.BLACK, 0.7f)
+    } else {
+        navigationBarColor
+    }
+
     fun enable() {
         activity.enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(statusBarColor),
-            navigationBarStyle = SystemBarStyle.light(
-                navigationBarColor,
-                navigationBarColor
-            )
+            statusBarStyle = styleFor(statusBarColor, useDarkStatusBarIcons),
+            navigationBarStyle = navigationBarStyle()
         )
     }
 
     fun applyInsets(binding: OcrActivityBinding) {
+        WindowCompat.getInsetsController(activity.window, activity.window.decorView).apply {
+            isAppearanceLightStatusBars = useDarkStatusBarIcons
+            isAppearanceLightNavigationBars = useDarkNavigationBarIcons
+        }
         binding.statusBarBackground.setBackgroundColor(statusBarColor)
+        binding.navigationBarBackground.setBackgroundColor(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                navigationBarColor
+            } else {
+                legacyNavigationBarColor
+            }
+        )
         ViewCompat.setOnApplyWindowInsetsListener(binding.fragmentContainerFl) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.updatePadding(
@@ -56,15 +78,17 @@ internal class OcrEdgeToEdgeHelper(
         ViewCompat.requestApplyInsets(binding.fragmentContainerFl)
     }
 
-    @ColorInt
-    private fun resolveThemeColor(@AttrRes attribute: Int): Int {
-        val value = TypedValue()
-        if (!activity.theme.resolveAttribute(attribute, value, true)) {
-            return ContextCompat.getColor(activity, R.color.purple_500)
+    private fun styleFor(@ColorInt color: Int, useDarkIcons: Boolean): SystemBarStyle =
+        if (useDarkIcons) {
+            SystemBarStyle.light(color, color)
+        } else {
+            SystemBarStyle.dark(color)
         }
-        return value.resourceId
-            .takeIf { it != 0 }
-            ?.let { ContextCompat.getColor(activity, it) }
-            ?: value.data
-    }
+
+    private fun navigationBarStyle(): SystemBarStyle =
+        if (useDarkNavigationBarIcons) {
+            SystemBarStyle.light(navigationBarColor, legacyNavigationBarColor)
+        } else {
+            SystemBarStyle.dark(navigationBarColor)
+        }
 }
